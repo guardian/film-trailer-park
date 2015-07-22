bfdf28b0b83e46fb9fd5e75092c4bd341b6fb07c
Sam Morris <mail@sammorrisdesign.co.uk>
2015-07-22 16:36:15 +0100

    basic switching

--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -21,36 +21,37 @@ define([
         },
 
         render: function() {
-            this.renderInfo();
             this.renderVideo();
+            this.renderInfo();
             Interactions.init();
+        },
+
+        renderVideo: function() {
+            var videoHtml = "";
+            $.each(data, function(index, value) {
+                var html = '<li class="trailer-playlist__item trailer-playlist__item--' + index + '" data-youtube="' + data[index].youtubeurl +'" style="background-image: url(' + data[index].imageurl + ');">' +
+                                '<div class="trailer-playlist__video"></div>' +
+                            '</li>';
+                videoHtml += html;
+            });
+            
+            $(".trailer-playlist").html(videoHtml);
         },
 
         renderInfo: function() {
             var infoHtml = "";
             $.each(data, function(index, value) {
-                var html = '<li class="trailer-body__item">' +
+                var html = '<li class="trailer-body__item trailer-body__item--' + index + '">' +
                                 '<h3 class="trailer-info__status">Now Playing</h3>' +
                                 '<h2 class="trailer-info__title">' + data[index].title + '</h2>' +
                                 '<div class="trailer-info__description">' +
                                     '<p>' + data[index].description + '</p>' +
                                 '</div>' +
                                 '<div class="trailer-info__button">Hey I\'m a button</div>' +
-                            '</li>"'
+                            '</li>'
                 infoHtml += html;
             });
             $(".trailer-body__list").html(infoHtml);
-        },
-
-        renderVideo: function() {
-            var videoHtml = "";
-            $.each(data, function(index, value) {
-                var html = '<li class="trailer-playlist__item" data-youtube="' + data[index].youtubeurl +'" style="background-image: url(' + data[index].imageurl + ')">' +
-                                '<div class="trailer-playlist__video"></div>' +
-                            '</li>';
-                videoHtml += html;
-            });
-            $(".trailer-playlist").html(videoHtml);
         }
     };
 });
